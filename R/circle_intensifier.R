2ae7c823a954b449b8b7437ec770c3db3689a0b3
KonstiDE <konstantin.mueller1@gmx.de>
2023-03-26 19:02:38 +0200

    dot map first sample

--- R/circle_intensifier.R
+++ R/circle_intensifier.R
@@ -18,13 +18,30 @@ plot_intensity_bubbles <- function(
   point_layer = st_read("data/ger_bakeries.gpkg")
   shape_layer = st_read("data/ger_admin.gpkg")
 
-  #point_layer_buffered = st_buffer(point_layer, 1000, endCapStyle = 'ROUND')
+  grid_crs <- crs(point_layer)
+
+  #point_layer_buffered = st_buffer(point_layer, 5000, endCapStyle = 'ROUND')
   #counts <- st_contains(point_layer_buffered, point_layer, sparse = F)
 
-  spatialrisk
+  coord_df <- st_coordinates(point_layer$geom)
+  coord_df <- coord_df[,1:2]
+  coord_df <- as.data.frame(coord_df)
+  colnames(coord_df) <- c("lon", "lat")
+  coord_df$lon <- as.numeric(coord_df$lon)
+  coord_df$lat <- as.numeric(coord_df$lat)
 
-  ggplot(data = point_layer, aes(fill=counts)) +
-    geom_sf() +
-    scale_fill_gradientn(colours = c("purple", "orange", "red"))
+  intensity_list <- numeric()
+  for (i in seq_along(coord_df[, 1])){
+    intensity_list <- append(
+      intensity_list,
+      length(spatialrisk::points_in_circle(coord_df, lon_center=coord_df$lon[i], lat_center=coord_df$lat[i], radius = 10000)[,1])
+    )
+  }
+
+  coord_sf <- st_as_sf(coord_df, coords = c("lon", "lat"), crs = grid_crs)
+
+  ggplot() +
+    geom_sf(data = coord_sf, aes(color=intensity_list, size = intensity_list)) +
+    scale_color_gradientn(colors = c("grey", "orange", "red"))
 
 }
